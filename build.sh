b6ea86c47497ff125dd75961f3a1e17a7fbdc7ec
Scott Alan Henry <shenry@phase2technology.com>
2020-12-02 18:18:22 -0600

    MASTER :: NOTICKET :: Update Dockerfile to use docksal/cli:2.12-php7.4
    * Update the cli version so it coincides with the tags in docksal/cli

--- build.sh
+++ build.sh
@@ -3,7 +3,15 @@
 set -e
 
 CLI_VERSION="2.10"
-for tag in {"php7.2" "php7.3"}; do
+for tag in {"php7.2"}; do
+  docker build \
+    --build-arg CLI_VERSION="${CLI_VERSION}-${tag}" \
+    -t outrigger/cli:${CLI_VERSION}-${tag} \
+    .
+done
+
+CLI_VERSION="2.12"
+for tag in {"php7.3" "php7.4"}; do
   docker build \
     --build-arg CLI_VERSION="${CLI_VERSION}-${tag}" \
     -t outrigger/cli:${CLI_VERSION}-${tag} \
