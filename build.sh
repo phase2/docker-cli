ccabb9b6f170dabbe37363fac9b4712a9344a160
Scott Alan Henry <shenry@phase2technology.com>
2020-12-02 18:33:13 -0600

    MASTER :: NOTICKET :: Alternate build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-CLI_VERSION="2.10"
-for tag in {"php7.2"}; do
+# Arguments: $1 tag, $2 CLI_VERSION
+build() {
   docker build \
-    --build-arg CLI_VERSION="${CLI_VERSION}-${tag}" \
-    -t outrigger/cli:${CLI_VERSION}-${tag} \
+    --build-arg CLI_VERSION="${2}-${1}" \
+    -t outrigger/cli:"${2}-${1}" \
     .
-done
+}
 
-CLI_VERSION="2.12"
-for tag in {"php7.3" "php7.4"}; do
-  docker build \
-    --build-arg CLI_VERSION="${CLI_VERSION}-${tag}" \
-    -t outrigger/cli:${CLI_VERSION}-${tag} \
-    .
+build "php7.2" "2.10"
+
+for tag in "php7.3" "php7.4"; do
+  build $tag "2.12"
 done
