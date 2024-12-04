e6d55af4998bef760d89b62dce8be1061645b2f4
Mike Potter <mpotter@phase2technology.com>
2024-12-04 15:49:53 -0800

    Fix handling of SQLite build argument

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@
 set -ex
 
 # Arguments: $1 CLI_VERSION
-docker build --build-arg CLI_VERSION=php"${1:-8.0}" -t phase2/docker-cli:php"${1:-8.0}" ./src
+CLI_VERSION=${1:-8.3}
+
+# Install SQLite 3.x for Drupal 11 testing if CLI version >= 8.3
+INSTALL_SQLITE=false
+if [ "$(echo "${CLI_VERSION} >= 8.3" | bc -l)" -eq 1 ]; then
+  INSTALL_SQLITE=true
+fi
+
+docker build --build-arg CLI_VERSION=php"$CLI_VERSION" --build-arg INSTALL_SQLITE="$INSTALL_SQLITE" -t phase2/docker-cli:php"$CLI_VERSION" ./src
